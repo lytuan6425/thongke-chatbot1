6a2cb79e8c9777894e24e907d4e24e3447ff917e
LÊ THU HẰNG <167497371+lytuan6425@users.noreply.github.com>
2024-04-25 16:02:28 +0700

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,1 +1,7 @@
+import PyPDF2
 
+with open('thongke.pdf', 'rb') as file:
+	reader = PyPDF2.PdfFileReader(file)
+	text = ''
+	for page in range(reader.numPages):
+		text += reader.getPage(page).extractText()
